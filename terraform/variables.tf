cfada1657f84d21492278e93d03658136cda7aae
JendyJasper <jendy36@gmail.com>
2023-08-27 19:48:19 +0000

    updated ami variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -39,7 +39,22 @@ variable "name" {
 
 }
 
-variable "ami" {
+variable "ami-web" {
+  type        = string
+  description = "AMI ID for the launch template"
+}
+
+variable "ami-bastion" {
+  type        = string
+  description = "AMI ID for the launch template"
+}
+
+variable "ami-nginx" {
+  type        = string
+  description = "AMI ID for the launch template"
+}
+
+variable "ami-sonar" {
   type        = string
   description = "AMI ID for the launch template"
 }
