c045a64ea6faa37f0e3ea006d5a9f1236fd45e3d
JendyJasper <jendy36@gmail.com>
2023-08-28 06:23:04 +0000

    updated bastion to run python 3.9

--- AMI/bastion.sh
+++ AMI/bastion.sh
@@ -3,9 +3,11 @@
 #!/bin/bash
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 sudo yum install -y dnf-utils http://rpms.remirepo.net/enterprise/remi-release-8.rpm 
-sudo yum install -y mysql-server wget vim telnet htop git python3 net-tools zip
+sudo yum install -y mysql-server wget vim telnet htop git python39 net-tools zip
 sudo systemctl start chronyd
 sudo systemctl enable chronyd
+sudo yum update -y
+sudo yum upgrade -y
 
 
 #installing java 11
@@ -20,17 +22,20 @@ git clone https://github.com/JendyJasper/terraform-cloud.git
 
 
 # install botocore, ansible and awscli
-sudo python3 -m pip install boto
-sudo python3 -m pip install boto3
-sudo python3 -m pip install PyMySQL
-sudo python3 -m pip install mysql-connector-python
-sudo python3 -m pip install --upgrade setuptools
-sudo python3 -m pip install --upgrade pip
-sudo python3 -m pip install psycopg2==2.7.5 --ignore-installed
+sudo python3.9 -m pip install boto
+sudo python3.9 -m pip install boto3
+sudo python3.9 -m pip install PyMySQL
+sudo python3.9 -m pip install mysql-connector-python
+sudo python3.9 -m pip install --upgrade setuptools
+sudo python3.9 -m pip install --upgrade pip
+sudo python3.9 -m pip install psycopg2==2.7.5 --ignore-installed
+sudo python3.9 -m pip install ansible
+sudo python3.9 -m pip install boto3
+sudo python3.9 -m pip install botocore
 sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo unzip awscliv2.zip
 sudo ./aws/install
-sudo yum install ansible -y
+#sudo yum install ansible -y
 sudo yum install -y policycoreutils-python-utils
 ansible-galaxy collection install amazon.aws
 ansible-galaxy collection install community.general
